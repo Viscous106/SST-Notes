764d98cf4050c3ef5cfb29494378325e3445f496
Viscous106 <virulkaryashed@gmail.com>
2025-08-18 16:15:34 +0530

    push

--- Java Notes/javaNotes.java
+++ Java Notes/javaNotes.java
@@ -8,8 +8,12 @@ public class javaNotes{
                System.out.println("Hello World");
 //if ln is wriitien after print it makes the next statement in new line
                System.out.println("Welcome to programming");
-	       System.out.print(5+9);
+	       System.out.println(5+9);
 //if written in "" then its string but if not it became operation or int or float or something depends on the value 
-                            
-       }
+              /*
+              System.out.println("hi"*"guys");   //error as string cant be mult
+	       System.out.println("hello"+3);//catenation
+              System.out.println(3*4+"Hello"+"World");//mult and catenation
+              */
+	}
 }
