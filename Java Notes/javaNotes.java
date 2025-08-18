5874dc52785f67245e69b86925888a2c5f2091ec
Viscous106 <virulkaryashed@gmail.com>
2025-08-18 16:04:25 +0530

    push

--- Java Notes/javaNotes.java
+++ Java Notes/javaNotes.java
@@ -1,6 +1,15 @@
+//This is used for a single line comment
+/*multiline comment
+to comment multiline*/
+
+
 public class javaNotes{
        public static void main (String[] args){
-               System.out.print("Hello World");
-               System.out.print("Welcome to programming");
+               System.out.println("Hello World");
+//if ln is wriitien after print it makes the next statement in new line
+               System.out.println("Welcome to programming");
+	       System.out.print(5+9);
+//if written in "" then its string but if not it became operation or int or float or something depends on the value 
+                            
        }
 }
